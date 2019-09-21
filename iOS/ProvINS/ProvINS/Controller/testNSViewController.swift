133bd7022dd98811b15539d4b2d917dabf46cbd9
s4rv4d <sarvadshetty@gmail.com>
2019-09-21 18:58:46 +0530

    bluetooth done

--- iOS/ProvINS/ProvINS/Controller/testNSViewController.swift
+++ iOS/ProvINS/ProvINS/Controller/testNSViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import MMLanScan
+import CoreBluetooth
 
-class testNSViewController: UIViewController, MMLANScannerDelegate {
+class testNSViewController: UIViewController, MMLANScannerDelegate,CBCentralManagerDelegate {
     
     var lanScanner : MMLANScanner!
+    var peripherals:[CBPeripheral] = []
+    var manager: CBCentralManager? = nil
+    let BLEService = "DFB0"
+    let BLECharacteristic = "DFB1"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,9 @@ class testNSViewController: UIViewController, MMLANScannerDelegate {
         // Do any additional setup after loading the view.
         print("entered here")
         self.lanScanner = MMLANScanner(delegate:self)
-        self.lanScanner.start()
+//        self.lanScanner.start()
+        manager = CBCentralManager(delegate: self, queue: nil);
+//        manager?.scanForPeripherals(withServices: [CBUUID.init(string:BLEService)], options: nil)
     }
     
     func lanScanDidFindNewDevice(_ device: MMDevice!) {
@@ -39,4 +46,36 @@ class testNSViewController: UIViewController, MMLANScannerDelegate {
         print("not able to scan")
     }
 
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("unknown")
+        case .resetting:
+            print("resetting")
+        case .unsupported:
+            print("unsupported")
+        case .unauthorized:
+            print("unauthorized")
+        case .poweredOff:
+            print("poweredOff")
+            manager!.stopScan()
+        case .poweredOn:
+            print("poweredOn")
+            manager?.scanForPeripherals(withServices: nil, options: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+//        print("peripheral \(peripheral.name)")
+        if peripheral.name == nil {
+            print("name not available")
+        } else {
+            print(peripheral.name)
+        }
+    }
+    
+    func stopScanForBLEDevice(){
+        manager?.stopScan()
+        print("scan stopped")
+    }
 }
