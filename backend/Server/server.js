b70971a5fb0d27d4c3d8aa801ac32f5d41ce288c
unknown <uddhav.navneeth@gmail.com>
2019-09-21 10:51:04 +0530

    backend changes

--- backend/Server/server.js
+++ backend/Server/server.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('default'));
+app.use(cors());
+
+app.get('/hey', (req,res) => {
+    res.send('Heyo!!');
+})
+
+app.listen(port, () => {
+    console.log(`started up at ${port}`);
+});
